ce690bb3699163ffb73e926077d30ca0478f9d6c
djechna <damian.jechna@gmail.com>
2023-11-10 18:33:45 +0100

    1.1 Second task completed

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -2,6 +2,8 @@ package lab2;
 public class Main {
     public static void main(String[] args) {
         Variables var = new Variables();    //utworzenie obiektu klasy Variables
+        var.conversion();   //(trochę zbędna)metoda do konwersji typów zmiennych
         var.drukuj();   //użycie metody z klasy do wypisania rezultatu na konsoli
+
     }
 }
--- src/lab2/Variables.java
+++ src/lab2/Variables.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Variables {
     //deklaracja zmiennych
     private int calkowita;
+    private double calkowitaDouble;
     private double zmienno;
+    private int zmiennoInt;
     private char znak;
+    private int znakInt;
     private boolean logiczna;
+    private String logicznaString;
+
     public Variables() {    //konstruktor obiektu klasy
         Scanner scanner = new Scanner(System.in);
 
@@ -26,15 +31,13 @@ public class Variables {
                 this.zmienno = scanner.nextDouble();
                 break;
             } catch (java.util.InputMismatchException e) {
-                System.out.print("Nieznany format. ");
+                System.out.print("Nieznany format. (przecinek/kropka) ");
                 scanner.nextLine();
             }
         }
 
-        String tmp; //Scanner nie ma wbudowanej metody do odczytu char-u, stąd tymczasowy String
         System.out.print("Podaj jeden(!) znak z klawiatury: ");
-        tmp=scanner.next();
-        this.znak = tmp.charAt(0);  //pobranie pierwszego znaku ze String-u
+        this.znak = scanner.next().charAt(0);  //pobranie pierwszego znaku z linii w konsoli
 
         while(true) {   //pętla sprawdzająca poprawność wprowadzonych danych przez użytkownika
             System.out.print("Wpisz \"ture\" lub \"false\": ");
@@ -48,7 +51,17 @@ public class Variables {
         }
         scanner.close();
     }
-    public void drukuj() {  //bardzo prosta metoda do testowania działania programu
-        System.out.printf(calkowita+"\t"+zmienno+"\t"+znak+"\t"+logiczna);
+    public void conversion() {
+        calkowitaDouble = calkowita; // Intellij twierdzi, że rzutowanie w tym przypadku jest zbędne
+        zmiennoInt = (int) Math.round(zmienno); // bez Math.round pominięta zostanie część liczby po przecinku
+        //Math.round jak sama nazwa wskazuje, zaokrągla liczbę. Nadal jest rzutowana, bo metoda ta zwraca long, nie int
+        znakInt = znak; // Intellij twierdzi, że rzutowanie w tym przypadku jest zbędne
+        logicznaString = String.valueOf(logiczna); // zmiennych logicznych nie można rzutować, stąd użycie metody
+    }
+    public void drukuj() {  //bardzo prosta metoda do wyświetlania danych
+        System.out.println("Wprowadzone dane:");
+        System.out.println(calkowita+"\t"+zmienno+"\t"+znak+"\t"+logiczna);
+        System.out.println("Dane przekonwertowane:");
+        System.out.println(calkowitaDouble+"\t"+zmiennoInt+"\t"+znakInt+"\t"+logicznaString);
     }
 }
